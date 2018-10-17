52103d351aa66860377845e896c0c749c0f4c25a
Taejoon Kim <tkim00@stuy.edu>
2018-10-17 11:02:50 -0400

    started working on writing the methods

--- RealNumber.java
+++ RealNumber.java
@@ -1,14 +1,49 @@
 public class RealNumber{
-	private double x;
+  private double value;
 
-	public RealNumber(double value){
-		x = value;
-	}
+  public RealNumber(double v){
+    value = v;
+  }
 
-	public double getX(){
-		return x;
-	}
-	public String toString(){
-		return "" + getX();
-	}
+  /*
+  *Return the sum of this and the other
+  */
+  public double add(RealNumber other){
+
+     //other can be ANY RealNumber, including a RationalNumber
+
+     //or other subclasses of RealNumber (that aren't written yet)
+
+     return value + other.value;
+  }
+
+  /*
+  *Return the product of this and the other
+  */
+  public double multiply(RealNumber other){
+    return 0;
+  }
+
+  /*
+  *Return the this divided by the other
+  */
+  public double divide(RealNumber other){
+    return 0;
+  }
+ 
+  /*
+  *Return the this minus the other
+  */
+  public double subtract(RealNumber other){
+    return 0;
+  }
+
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+value;
+  }
 }
